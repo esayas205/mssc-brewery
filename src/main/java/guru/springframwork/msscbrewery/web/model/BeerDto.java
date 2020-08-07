12f7e7e546dd3ef74104300040ac947ad6040b2d
Esayas Bekele <esayas82@gmail.com>
2020-08-07 14:37:29 -0500

    Updating data model

--- src/main/java/guru/springframwork/msscbrewery/web/model/BeerDto.java
+++ src/main/java/guru/springframwork/msscbrewery/web/model/BeerDto.java
@@ -1,11 +1,12 @@
 package guru.springframwork.msscbrewery.web.model;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Data
@@ -15,6 +16,11 @@ import java.util.UUID;
 public class BeerDto {
     private UUID id;
     private String beerName;
-    private String beerStyle;
+    private BeerStyleEnum beerStyle;
     private Long ups;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+
+    private OffsetDateTime createdAt;
+    private OffsetDateTime updatedAt;
 }
